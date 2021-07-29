1fbc141a9a1419cc8f0c55f645c0d7cafdf7c596
solvedDev <solvedDev@gmail.com>
2021-07-29 16:33:51 +0200

    upd: zipFolder script

--- scripts/components/Zip/ZipFolder.ts
+++ scripts/components/Zip/ZipFolder.ts
@@ -1,23 +1,21 @@
-import { join } from 'path'
+import { join, basename } from 'path'
 import { zipSync } from 'fflate'
 
-export async function zipFolder(
-	path: string,
-	outputPath: string,
-	globalFolder?: string
-) {
+export async function zipFolder(path: string, outputPath: string) {
 	const dirContents = {}
 	await getDirectoryContents(path, dirContents)
 
-	const zipped = zipSync(
-		globalFolder ? { [globalFolder]: dirContents } : dirContents
-	)
+	const zipped = zipSync(dirContents)
+	console.log(Object.keys(dirContents))
 
 	await Deno.writeFile(outputPath, zipped)
 }
 
 async function getDirectoryContents(path: string, dirContents: any) {
-	if (path === './dist') path = './packages'
+	if (path === './dist') {
+		path = './packages'
+		dirContents['packages/'] = new Uint8Array()
+	}
 
 	for await (const entry of Deno.readDir(path)) {
 		const entryPath = join(path, entry.name)
@@ -25,7 +23,7 @@ async function getDirectoryContents(path: string, dirContents: any) {
 		if (entry.isDirectory) {
 			dirContents[entryPath + '/'] = new Uint8Array()
 			await getDirectoryContents(entryPath, dirContents)
-		} else {
+		} else if (basename(entryPath) !== '.DS_Store') {
 			dirContents[entryPath] = await Deno.readFile(entryPath)
 		}
 	}
