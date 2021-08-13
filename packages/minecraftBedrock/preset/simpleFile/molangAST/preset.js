767ab5f58e2b45fc6c08ba000ba1202beb7034f0
Joelant05 <Joelant05@users.noreply.github.com>
2021-08-13 20:15:13 +0100

    feat: Molang AST preset

---
+++ packages/minecraftBedrock/preset/simpleFile/molangAST/preset.js
@@ -0,0 +1,11 @@
+module.exports = async ({ createFile, loadPresetFile, models }) => {
+	const { LANGUAGE, FILE_NAME } = models
+	const file = await loadPresetFile('script.js')
+
+	await createFile(
+		`BP/scripts/molang/${FILE_NAME}.${
+			LANGUAGE === 'JavaScript' ? 'js' : 'ts'
+		}`,
+		file
+	)
+}
