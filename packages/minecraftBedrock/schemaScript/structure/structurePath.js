576d645c12e5f6c4365f26843778ec9a820d8ff3
Joelant05 <Joelant05@users.noreply.github.com>
2021-10-26 21:03:55 +0100

    feat: support dynamic pack paths in schema scripts
    bridge-core/editor#200

--- packages/minecraftBedrock/schemaScript/structure/structurePath.js
+++ packages/minecraftBedrock/schemaScript/structure/structurePath.js
@@ -1,12 +1,12 @@
 let structureIdentifiers = []
 let structurePaths = []
-await readdir('BP/structures').then((dirents) =>
+await readdir(resolvePackPath('behaviorPack', 'structures')).then((dirents) =>
 	dirents.forEach((dirent) => structurePaths.push(dirent.path))
 )
 
 for (const path of structurePaths) {
 	const strippedPath = path
-		.replace('BP/structures/', '')
+		.replace(`${resolvePackPath('behaviorPack', 'structures')}/`, '')
 		.replace('.mcstructure', '')
 	const pathParts = strippedPath.split('/')
 
