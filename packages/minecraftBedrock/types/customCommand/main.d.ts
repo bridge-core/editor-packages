e5bf269ef25a927adab8770b2a137b1d14fbf019
solvedDev <solvedDev@gmail.com>
2021-11-21 19:47:12 +0100

    feat: new custom command APIs

--- packages/minecraftBedrock/types/customCommand/main.d.ts
+++ packages/minecraftBedrock/types/customCommand/main.d.ts
@@ -12,5 +12,20 @@ declare interface CommandContext<T> {
 }
 
 declare interface TemplateContext {
+	/**
+	 * The current compiler mode under which the custom component gets compiled
+	 */
 	compilerMode: 'build' | 'dev'
+	/**
+	 * How deep within other commands this custom command is nested
+	 *
+	 * @example /execute @a ~ ~ ~ myCustomCommand - commandNestingDepth = 1
+	 * @example /myCustomCommand - commandNestingDepth = 0
+	 */
+	commandNestingDepth: number
+	/**
+	 * Compile custom commands that are nested within the current custom command
+	 * Allows to e.g. create custom /execute functions
+	 */
+	compileCommands: (commands: string[]) => string[]
 }
