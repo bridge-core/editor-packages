d18d1ac6ed28393004368270180e49de555f4582
solvedDev <solvedDev@gmail.com>
2022-01-02 16:56:59 +0100

    upd: commit zip size

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,14 +1,21 @@
 import { zipFolder } from './components/Zip/ZipFolder.ts'
 import { packageDirectory } from './components/Packager/Packager.ts'
 
-export async function build(outDir: string) {
+export async function build(outputPath: string) {
 	try {
 		await Deno.remove('./dist')
 		await Deno.mkdir('./dist', { recursive: true })
 	} catch {}
 
 	await packageDirectory('./packages', './dist')
-	await zipFolder('./dist', outDir)
+	await zipFolder('./dist', outputPath)
+
+	// Get ZIP size
+	const stat = await Deno.stat(outputPath)
+	await Deno.writeTextFile(
+		'./dataPackage.ts',
+		`export const zipSize = ${stat.size}`
+	)
 
 	// for await (const entry of Deno.readDir('./packages')) {
 	// 	if (entry.isDirectory)
