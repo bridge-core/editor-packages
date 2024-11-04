112ebac4b498ba5b30df449b801578d8947ead56
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-11-04 15:57:41 -0500

    Fix structurePathEnum

--- packages/minecraftBedrock/schemaScript/structure/structurePath.js
+++ packages/minecraftBedrock/schemaScript/structure/structurePath.js
@@ -1,17 +1,19 @@
-let structurePaths = []
+const structurePaths = []
+const strippedPaths = []
 
 await readdir(resolvePackPath('behaviorPack', 'structures')).then((dirents) =>
     dirents.forEach((dirent) => structurePaths.push(dirent.path))
 )
 
 for (const path of structurePaths) {
-    path = path
+    const strippedPath = path
         .replace(`${resolvePackPath('behaviorPack', 'structures')}/`, '')
         .replace('.mcstructure', '')
+    strippedPaths.push(strippedPath)
 }
 
 return {
     type: 'enum',
     generateFile: 'structure/dynamic/structurePathEnum.json',
-    data: structurePaths,
+    data: strippedPaths,
 }
