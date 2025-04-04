7bf2da2c5369cfad8b40b96833e6fd78b528ed30
ExDrill <harblast123@gmail.com>
2025-04-03 20:34:41 -0500

    Add new fields to Jigsaw Structures

---
+++ packages/minecraftBedrock/schemaScript/general/targetVersion/preview.js
@@ -0,0 +1,9 @@
+const config = await getProjectConfig()
+
+return {
+    type: 'custom',
+    generateFile: 'project/targetVersion/preview.json',
+    data: {
+        if: config.targetVersion && config.targetVersion == '1.21.80'
+    }
+}
