38a36c1016a4e0dac46da3ad7caabca0bda0cb18
ExDrill <harblast123@gmail.com>
2026-04-27 22:25:51 -0500

    1.26.10 stable

--- packages/minecraftBedrock/schemaScript/general/targetVersion/preview.js
+++ packages/minecraftBedrock/schemaScript/general/targetVersion/preview.js
@@ -4,6 +4,6 @@ return {
 	type: 'custom',
 	generateFile: 'project/targetVersion/preview.json',
 	data: {
-		if: config.targetVersion && config.targetVersion == '1.21.130',
+		if: config.targetVersion && config.targetVersion == '1.26.20',
 	},
 }
